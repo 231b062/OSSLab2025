8e680c2e320d7526d7790397def3ca1ae844ad5c
231B062 <231B062@juet.co.in>
2025-08-01 10:46:47 +0530

    Lab 1 solution

---
+++ LE1/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+     public static void main(String args[])
+     {
+              int a = 20 , b = 25;
+              if(a == b ) System.out.println("Both are Equal");
+              else if(a > b) System.out.println(a + " is greater");
+              else System.out.println(b+" is greater");
+
+     }
+}
